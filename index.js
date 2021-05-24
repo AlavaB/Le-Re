46a2102934992a9a2f539eb01acae5702f0793e8
Emilie Bojkovic <emilie.bojkovic@outlook.com>
2021-05-24 19:18:25 +0200

    Chore: Welcome page construction setting up content

---
+++ index.js
@@ -0,0 +1,14 @@
+let navigation = document.getElementById("navigation");
+let sticky = navigation.offsetTop;
+
+function stickyNavigation() {
+  if (window.pageYOffset > sticky) {
+    navigation.classList.add("sticky");
+  } else {
+    navigation.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function () {
+  stickyNavigation();
+};
